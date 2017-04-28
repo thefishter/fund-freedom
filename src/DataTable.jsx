e59e23d1c8c29a5aff4e22e3412039d0fe1c0978
Nicole Fish <nicole.fish@gmail.com>
2017-04-28 12:38:40 -0400

    Create DataTable component that renders API data pulls with formatted dates and monetary amounts

---
+++ src/DataTable.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const DataTable = (props) => {
+
+  const inmates = props.inmates
+
+  return (
+    <div className="DataTable">
+      <table>
+      
+        <thead>
+          <tr>
+            <th>Download Date</th>
+            <th>Identifier</th>
+            <th>Gender</th>
+            <th>Race</th>
+            <th>Age</th>
+            <th>Bail</th>
+            <th>Offense</th>
+            <th>Latest Admission Date</th>
+            <th>Facility</th>
+            <th>Detainer</th>
+          </tr>
+        </thead>
+      
+        <tbody>
+        {
+          inmates.map(inmate => (
+            <tr>
+              <td>{inmate.download_date.slice(0, 10)}</td>
+              <td>{inmate.identifier}</td>
+              <td>{inmate.gender}</td>
+              <td>{inmate.race}</td>
+              <td>{inmate.age}</td>
+              <td>{ Math.floor(inmate.bond_amount / 1000)
+                    ? Math.floor(inmate.bond_amount / 1000000)
+                      ? '$' + inmate.bond_amount.slice(0, -6) + ',' 
+                            + inmate.bond_amount.slice(-6, -3) + ','
+                            + inmate.bond_amount.slice(-3)
+                      : '$' + inmate.bond_amount.slice(0, -3) + ','
+                            + inmate.bond_amount.slice(-3)
+                    : '$' + inmate.bond_amount }</td>
+              <td>{inmate.offense}</td>
+              <td>{inmate.latest_admission_date.slice(0, 10)}</td>
+              <td>{inmate.facility}</td>
+              <td>{inmate.detainer}</td>
+            </tr>
+          ))
+        }
+        </tbody>
+      
+      </table>
+    </div>
+  )
+}
+export default DataTable
