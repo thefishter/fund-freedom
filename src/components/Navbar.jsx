c3328dfcb41ce60d4ffa51c2023bd8f73a8d495b
Nicole Fish <nicole.fish@gmail.com>
2017-04-30 19:49:03 -0400

    Update Navbar.jsx
    refactored to dumb component; removed unused handleSelect function and { Component } import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Nav, NavItem } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
-class Navbar extends Component {
-
-  handleSelect(eventKey) {
-    event.preventDefault()
-    alert(`selected ${eventKey}`)
-  }
-
-  render() {
-    return (
-      <Nav bsStyle="tabs">
-        <LinkContainer to="/about">
-          <NavItem>About</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/visualize">
-          <NavItem>Interactive Visualization</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/datatable">
-          <NavItem>Real Time Dataset</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/trends">
-          <NavItem>Highlighted Trends</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/donate">
-          <NavItem>Donate</NavItem>
-        </LinkContainer>
-      </Nav>
-    )
-  }
-
+const Navbar = (props) => {
+  return (
+    <Nav bsStyle="tabs">
+      <LinkContainer to="/about">
+        <NavItem>About</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/visualize">
+        <NavItem>Interactive Visualization</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/datatable">
+        <NavItem>Real Time Dataset</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/trends">
+        <NavItem>Highlighted Trends</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/donate">
+        <NavItem>Donate</NavItem>
+      </LinkContainer>
+    </Nav>
+  )
 }
 
 export default Navbar
