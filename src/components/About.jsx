7745c9a3297c734ef308160697bb930889373d52
Nicole Fish <nicole.fish@gmail.com>
2017-05-01 15:00:58 -0400

    add about text; format with side margins

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 const About = (props) => {
   return (
     <div className="About">
-      <p>Why should I care?</p>
+    <br/>
+    <div className="singletonText">
+      <h4>Bail creates an intractable system of wealth-based jailing</h4>
+      <p>Every year, thousands of innocent people are sent to jail simply because they can’t afford to post bail, putting them at risk of losing their jobs, custody of their children — even their lives.</p>
+
+      <p>This costs the taxpayer as well. In NYC alone, roughly 45,000 people are jailed each year for the inability to pay bail. In 2013, the average stay in a New York City jail was 24 days, for which the city paid $450 per day per inmate.</p>
+      <br/>
+
+      <h4> Bail acts as a tool of compulsion</h4>
+      <p>Not only does being jailed increase the risk of hardship, violence, and long-term damage inflicted on vulnerable defendants, those who are jailed pre-trial due to the inability to post bail are more often than not forced to plead guilty. A multivariate analysis in a 2012 report by the NYC Criminal Justice Agency (based on 10 years of criminal statistics) showed that detaining defendants before their trials due to inability to pay bail increased the conviction rate from around 50% to 92%. Detention itself creates enough pressure to increase guilty pleas.</p>
+
+      <p>High profile tragedies have brought the eradication of bail to the forefront of criminal justice reform, such as that of Kalief Browder, a Bronx teenager who killed himself after being held on bail for three years in Rikers... for being suspected of stealing a backpack. His case was later dismissed.</p>
+
+      <p>Even for those whose family members can scrape together enough money for bail, byzantine systems make it impossible to do so promptly. Despite being innocent in the eyes of the law, these defendants end up serving time.</p>
+      <br/>
+
+      <h4>Don't wait for bail reform — help now</h4>
+      <p>But you can help ensure pretrial justice for your neighbors now. In 2013, nearly 16,000 people could not make bail set at $2,000 or less for nonviolent offenses — of those, 6,000 people were unable to make bail set between $20 to $500. Your participation is crucial - as bondsmen can't make enough money from small bails to make it worth their time, for these low bail amounts, the defendents are usually on their own.</p>
+
+      <p>Help us end wealth-based jailing, and secure their freedom.</p>
+    </div>
     </div>
   )
 }
